c2bb1af3765d007b60f4ba73539cfaf4e1be02e9
micha <micha@DESKTOP-E4EPNIN>
2017-07-17 19:34:00 +0200

    sadf

--- src/main/java/marolt/it/services/CDService.java
+++ src/main/java/marolt/it/services/CDService.java
@@ -2,6 +2,28 @@ package marolt.it.services;
 
 public class CDService {
 
+	double price;
+	double percentage;
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public void setPercentage(double percentage) {
+		this.percentage = percentage;
+	}
+	
+	// Ultimative BusinessLogic
+	public double calcPercentage()
+	{
+		double zahl = percent2Double(this.percentage);
+		return multiply(this.price, zahl);
+	}
+	
+	
+
+	// Methoden af
+	
 	public double multiply(double a, double b){
 		return a*b;
 	}
@@ -9,4 +31,6 @@ public class CDService {
 	public double percent2Double(double percent) {
 		return percent/100;
 	}	
+	
+	
 }
